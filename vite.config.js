2754f00d57fcd664644b910a126cd9a33fd9a070
Kevin Bett <prodigy4614@gmail.com>
2025-01-27 11:48:34 +0300

    update vite.config.js file host interfaces

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 3000, // Use the PORT env variable if available
+    port: process.env.PORT || 3000,  // Use the PORT environment variable or default to 3000
+    host: '0.0.0.0',  // Bind to all interfaces, necessary for containerized environments
   }
 })
