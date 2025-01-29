169309ad10d0abd7df448001d1bab7af9b8f92df
Kevin Bett <prodigy4614@gmail.com>
2025-01-29 14:24:22 +0300

    update api endpoint

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const apiUrl = "http://localhost:5000"; // Change this to your Flask backend URL
+const apiUrl = "http://127.0.0.1:5000"; // Change this to your Flask backend URL
 
 export default apiUrl;
