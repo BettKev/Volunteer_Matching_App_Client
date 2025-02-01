29da7427f9208f1de81ca1080de04ce86b867dfe
Kevin Bett <prodigy4614@gmail.com>
2025-02-01 11:29:01 +0300

    update api endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // Uncomment to use required production or development environments
 
-const apiUrl = "http://127.0.0.1:5000"; // localhost backend URL
-// const apiUrl = "https://volunteer-matching-app-server.onrender.com/"; // Remote backend URL
+// const apiUrl = "http://127.0.0.1:5000"; // localhost backend URL
+const apiUrl = "https://volunteer-matching-app-server.onrender.com/"; // Remote backend URL
 export default apiUrl;
